2fa9b28c4df07081332e0d9cf89fcdf4b9b430f6
Ali Farhadnia <ali.farhadnia.80@gmail.com>
2021-12-27 01:16:18 +0330

    edit config for debuging

--- serverGRPC/config/config.go
+++ serverGRPC/config/config.go
@@ -31,50 +31,50 @@ func (a *AppConfig) SetEnvVar() error {
 	//set grpc env var
 	env_var = os.Getenv("grpc_address")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:grpc_address")
 	}
 	a.GrpcConfig.Address = env_var
 
 	env_var = os.Getenv("grpc_network")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:grpc_network")
 	}
 	a.GrpcConfig.Network = env_var
 	//set database env var
 	//set db_user
 	env_var = os.Getenv("db_user")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:db_user")
 	}
 	a.DbConfig.User = env_var
 	//set db_password
 	env_var = os.Getenv("db_password")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:db_password")
 	}
 	a.DbConfig.Password = env_var
 	//set db_sslmode
 	env_var = os.Getenv("db_sslmode")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:db_sslmode")
 	}
 	a.DbConfig.Sslmode = env_var
 	//set db_host
 	env_var = os.Getenv("db_host")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:db_host")
 	}
 	a.DbConfig.Host = env_var
 	//set db_port
 	env_var = os.Getenv("db_port")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input:db_port")
 	}
 	a.DbConfig.Port = env_var
 	//set db_name
 	env_var = os.Getenv("db_name")
 	if env_var == "" {
-		return errors.New("empty env var input")
+		return errors.New("empty env var input :db_name")
 	}
 	a.DbConfig.DbName = env_var
 
