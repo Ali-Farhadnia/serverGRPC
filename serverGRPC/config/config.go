b920aa91d3054005488c95fa28c5b9bd4bcddefa
Ali Farhadnia <ali.farhadnia.80@gmail.com>
2021-12-26 19:13:40 +0330

    remove defult mode

--- serverGRPC/config/config.go
+++ serverGRPC/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"encoding/json"
 	"errors"
 	"os"
 )
@@ -28,82 +27,56 @@ func NewAppConfig() *AppConfig {
 }
 
 func (a *AppConfig) SetEnvVar() error {
-	var defult_cfg = NewAppConfig()
-	var defult_use_flag = false
-	//unmarshal defult config from defult_config.json
-	file, err := os.ReadFile("./defult_config.json")
-
-	json.Unmarshal(file, defult_cfg)
 	var env_var string
-	var ok bool
 	//set grpc env var
-	env_var, ok = os.LookupEnv("grpc_address")
-	if !ok {
-		a.GrpcConfig.Address = defult_cfg.GrpcConfig.Address
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("grpc_address")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.GrpcConfig.Address = env_var
 
-	env_var, ok = os.LookupEnv("grpc_network")
-	if !ok {
-		a.GrpcConfig.Network = defult_cfg.GrpcConfig.Network
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("grpc_network")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.GrpcConfig.Network = env_var
 	//set database env var
 	//set db_user
-	env_var, ok = os.LookupEnv("db_user")
-	if !ok {
-		a.DbConfig.User = defult_cfg.DbConfig.User
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("db_user")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.DbConfig.User = env_var
 	//set db_password
-	env_var, ok = os.LookupEnv("db_password")
-	if !ok {
-		a.DbConfig.Password = defult_cfg.DbConfig.Password
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("db_password")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.DbConfig.Password = env_var
 	//set db_sslmode
-	env_var, ok = os.LookupEnv("db_sslmode")
-	if !ok {
-		a.DbConfig.Sslmode = defult_cfg.DbConfig.Sslmode
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("db_sslmode")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.DbConfig.Sslmode = env_var
 	//set db_host
-	env_var, ok = os.LookupEnv("db_host")
-	if !ok {
-		a.DbConfig.Host = defult_cfg.DbConfig.Host
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("db_host")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.DbConfig.Host = env_var
 	//set db_port
-	env_var, ok = os.LookupEnv("db_port")
-	if !ok {
-		a.DbConfig.Port = defult_cfg.DbConfig.Port
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("db_port")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.DbConfig.Port = env_var
 	//set db_name
-	env_var, ok = os.LookupEnv("db_name")
-	if !ok {
-		a.DbConfig.DbName = defult_cfg.DbConfig.DbName
-		defult_use_flag = true
-	} else if env_var == "" {
+	env_var = os.Getenv("db_name")
+	if env_var == "" {
 		return errors.New("empty env var input")
 	}
+	a.DbConfig.DbName = env_var
 
-	if defult_use_flag && err != nil {
-		return err
-	}
 	return nil
 }
